e58f7c8f409e668142e31c17e38a680d12ee349a
KanG98 <yankang198@gmail.com>
2022-03-27 15:08:07 -0400

    set up basic socket io

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const morgan = require('morgan');
-
 const app = express();
+const server = require('http').createServer(app)
+const path = require('path');
+const socketio = require('socket.io')
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'client')))
+
+const io = socketio(server)
+
 
 
 app.get('/', (req, res) =>{
-    res.send("hello world")
+  res.render('index')
 })
+
+io.on('connection', (socket) => {
+  console.log('new socket io connection', socket.id)
+
+  // emit to one user
+
+  // socket.emit('message', "Welcome to K-Chat!") // send message to front end 
+
+  // emit to everybody except you
+
+  socket.broadcast.emit('message', 'a user has joined the chat')
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat')
+  })
+    
+  // emit to everybody
+  // io.emit()
+
+})
+
+
 
 const port = process.env.PORT || 3030;
 
-app.listen(port, () => 
+server.listen(port, () => 
   console.log(`App is listening on port ${port}.`)
 );
+
