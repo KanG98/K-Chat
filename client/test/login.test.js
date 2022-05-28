5df8a46bc4f86c73c9963d7124a8894f796f5db1
KanG98 <yankang198@gmail.com>
2022-05-27 20:38:11 -0400

    finish login test

--- client/test/login.test.js
+++ client/test/login.test.js
@@ -1,147 +1,147 @@
 const puppeteer = require('puppeteer')
 
-// test('should login successfully', async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 80,
-//     args: ['--window-size=900,1080']
-//   });
+test('should login successfully', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
 
-//   const page = await browser.newPage();
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
 
-//   await page.type('#email', 'test');
-//   await page.type('#password', 'test');
-//   await page.click('.login-button')
+  await page.type('#email', 'test');
+  await page.type('#password', 'test');
+  await page.click('.login-button')
 
-//   const firstname = await page.$$eval('.user-firstname', messages => {
-//     return messages.map(option => option.textContent)
-//   })
+  const firstname = await page.$$eval('.user-firstname', messages => {
+    return messages.map(option => option.textContent)
+  })
 
-//   const lastname = await page.$$eval('.user-lastname', messages => {
-//     return messages.map(option => option.textContent)
-//   })
+  const lastname = await page.$$eval('.user-lastname', messages => {
+    return messages.map(option => option.textContent)
+  })
 
-//   await browser.close()
-//   expect(firstname.concat(lastname) == ['me', 'me'])
+  await browser.close()
+  expect(firstname.concat(lastname) == ['me', 'me'])
 
-// }, 100000)
+}, 100000)
 
-// test('should logout', async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 80,
-//     args: ['--window-size=900,1080']
-//   });
+test('should logout', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
 
-//   const page = await browser.newPage();
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
 
-//   await page.type('#email', 'test')
-//   await page.type('#password', 'test')
-//   await page.click('.login-button')
-//   await page.click('.logout-button')
+  await page.type('#email', 'test')
+  await page.type('#password', 'test')
+  await page.click('.login-button')
+  await page.click('.logout-button')
 
-//   const url = page.url()
+  const url = page.url()
 
-//   await browser.close()
-//   expect(url === "http://localhost:3030/user/login")
+  await browser.close()
+  expect(url === "http://localhost:3030/user/login")
 
-// }, 100000)
-
-
-// test('should have session on', async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 80,
-//     args: ['--window-size=900,1080']
-//   });
-
-//   const page = await browser.newPage();
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
-
-//   await page.type('#email', 'test');
-//   await page.type('#password', 'test');
-//   await page.click('.login-button')
-
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
-
-//   const firstname = await page.$$eval('.user-firstname', messages => {
-//     return messages.map(option => option.textContent)
-//   })
-
-//   const lastname = await page.$$eval('.user-lastname', messages => {
-//     return messages.map(option => option.textContent)
-//   })
-
-//   await browser.close()
-//   expect(firstname.concat(lastname) == ['me', 'me'])
-
-// }, 100000)
-
-// test('should have session off', async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 80,
-//     args: ['--window-size=900,1080']
-//   });
-
-//   const page = await browser.newPage();
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
-
-//   await page.type('#email', 'test');
-//   await page.type('#password', 'test');
-//   await page.click('.login-button')
-//   await page.click('.logout-button')
-
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
+}, 100000)
 
 
-//   const url = await page.url()
-//   await browser.close()
+test('should have session on', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
 
-//   expect(url == 'http://localhost:3030/user/login')
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
 
-// }, 100000)
+  await page.type('#email', 'test');
+  await page.type('#password', 'test');
+  await page.click('.login-button')
 
-// test('should restrict to have same session ', async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 80,
-//     args: ['--window-size=900,1080']
-//   });
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
 
-//   const page = await browser.newPage();
-//   await page.goto(
-//     'http://localhost:3030/user/login'
-//   )
+  const firstname = await page.$$eval('.user-firstname', messages => {
+    return messages.map(option => option.textContent)
+  })
 
-//   await page.type('#email', 'test'); // test account id is ae724512-b73d-4cf9-ac35-6a17f4015f56
-//   await page.type('#password', 'test');
-//   await page.click('.login-button')
+  const lastname = await page.$$eval('.user-lastname', messages => {
+    return messages.map(option => option.textContent)
+  })
 
-//   await page.goto(
-//     'http://localhost:3030/me/c0d871b5-0f4e-4bb5-acf0-b3c93e3ae4fa'
-//   )
+  await browser.close()
+  expect(firstname.concat(lastname) == ['me', 'me'])
 
-//   const url = await page.url()
-//   await browser.close()
+}, 100000)
 
-//   expect(url == 'http://localhost:3030/me/ae724512-b73d-4cf9-ac35-6a17f4015f56')
+test('should have session off', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
 
-// }, 100000)
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
+
+  await page.type('#email', 'test');
+  await page.type('#password', 'test');
+  await page.click('.login-button')
+  await page.click('.logout-button')
+
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
+
+
+  const url = await page.url()
+  await browser.close()
+
+  expect(url == 'http://localhost:3030/user/login')
+
+}, 100000)
+
+test('should restrict to have same session ', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
+
+  await page.type('#email', 'test'); // test account id is ae724512-b73d-4cf9-ac35-6a17f4015f56
+  await page.type('#password', 'test');
+  await page.click('.login-button')
+
+  await page.goto(
+    'http://localhost:3030/me/c0d871b5-0f4e-4bb5-acf0-b3c93e3ae4fa'
+  )
+
+  const url = await page.url()
+  await browser.close()
+
+  expect(url == 'http://localhost:3030/me/ae724512-b73d-4cf9-ac35-6a17f4015f56')
+
+}, 100000)
 
 
 test('should have session expired', async () => {
