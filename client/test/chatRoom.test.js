e90299940a895b03330892af6c74ca642a22e29b
KanG98 <yankang198@gmail.com>
2022-05-28 23:24:53 -0400

    create chatRoom session test

--- client/test/chatRoom.test.js
+++ client/test/chatRoom.test.js
@@ -47,3 +47,44 @@ test('should send and receive message', async () => {
   expect(finalText1.concat(finalText2)).toStrictEqual(['user2 has joined the chat', 'Cindy', 'Cindy', 'Kyle', 'user1 user has left the chat']); 
 
 }, 100000)
+
+
+test('should go to main page when there is no session', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/chat/user1/room1'
+  );
+  browser.close()
+  expect(page.url == "http://localhost:3030/user/login")
+
+}, 100000)
+
+test('should go to main when the session is logged out', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=900,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:3030/user/login'
+  )
+
+  await page.type('#email', 'test');
+  await page.type('#password', 'test');
+  await page.click('.login-button')
+
+
+  await page.click('.logout-button')
+
+  await page.goto(
+    'http://localhost:3030/chat/ae724512-b73d-4cf9-ac35-6a17f4015f56/942efb83-5277-474a-b64b-f0b927c1a051'
+  )
+  browser.close()
+  expect(page.url == "http://localhost:3030/user/login")
+}, 100000)
