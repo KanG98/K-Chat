47a1d6a1fa56c96f24b49de93eecbdd264da9c0a
KanG98 <yankang198@gmail.com>
2022-05-29 20:53:20 -0400

    add theme template

--- client/js/mePageEvents.js
+++ client/js/mePageEvents.js
@@ -2,3 +2,10 @@ function dropdownEvent(){
   document.getElementsByClassName('dropdown-content')[0].classList.toggle("show")
 }
 
+function createRoom(){
+  console.log("create room")
+  // generate field for info 
+  // the form submit will ping backend create room api
+
+}
+
