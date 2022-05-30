424cc9953c7ad59f55f2d3cb5c655b9db8c6128c
KanG98 <yankang198@gmail.com>
2022-05-29 21:21:41 -0400

    add style to me page

--- client/js/mePageEvents.js
+++ client/js/mePageEvents.js
@@ -9,3 +9,16 @@ function createRoom(){
 
 }
 
+window.onclick = function(event) {
+  if (!event.target.matches('.user-firstname')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
