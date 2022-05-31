200d10e09123cf25062ba779a553aaffb607fad2
KanG98 <yankang198@gmail.com>
2022-05-31 00:57:59 -0400

    complete delete joinRoom

--- client/js/getUserRooms.js
+++ client/js/getUserRooms.js
@@ -16,23 +16,23 @@ function getOtherRoom(userId){
       mapRoomList(userId, res, "other-room-list")
     })
     .catch(function (error) {
-      // handle error
       console.log(error);
     })
 }
 
 function mapRoomList(userId, rooms, className){
   const myRoomListElm = document.getElementsByClassName(className)[0]
+  // rooms first item is the user info
   rooms.map((room) => {
     const liDiv = document.createElement('div')
     liDiv.className = "room-li-container"
     const li = document.createElement('div')
     li.className = "room-li"
-    // li.appendChild(document.createTextNode(room['roomName'])) 
-    li.appendChild(document.createTextNode(room['roomName'])) 
+    const roomName = className == 'my-room-list' ? room['roomName'] : room['room']['roomName']
+    li.appendChild(document.createTextNode(roomName)) 
     li.appendChild(document.createElement('br')) 
     li.appendChild(document.createTextNode(room['roomId']))
-    
+
     const deleteLi = document.createElement('div')
     deleteLi.className = `delete-room-btn_${room['roomId']}`
     deleteLi.appendChild(document.createTextNode('delete'))
@@ -46,9 +46,17 @@ function mapRoomList(userId, rooms, className){
       document.location.href = `http://localhost:3030/chat/${userId}/${room['roomId']}`
     })
 
-    deleteLi.addEventListener('click', (e) => {
-      const roomId = e.target.className.split('_')[1]
-      fetch(`/room/delete/${roomId}`, { method: 'DELETE' }).then(() => {document.location.reload()})
-    })
+        
+    if(className == 'my-room-list'){
+      deleteLi.addEventListener('click', (e) => {
+        const roomId = e.target.className.split('_')[1]
+        fetch(`/room/delete/${roomId}`, { method: 'DELETE' }).then(() => {document.location.reload()})
+      })
+    }else if (className == 'other-room-list'){
+      deleteLi.addEventListener('click', (e) => {
+        const roomId = e.target.className.split('_')[1]
+        fetch(`/joinRoom/delete/${userId}/${roomId}`, { method: 'DELETE' }).then(() => {document.location.reload()})
+      })
+    }
   }) 
 }
--- database/DMLs/getJoinRoom.js
+++ database/DMLs/getJoinRoom.js
@@ -1,7 +1,15 @@
 async function getJoinRoom(request){
+  console.log(request)
   const JoinRoom = require('../joinRoom')
-  const res = await JoinRoom.findAll({ where: request})
-  const resRooms = res.map( (room) => room['dataValues'])
+  const Rooms = require('../rooms')
+
+  Rooms.hasMany(JoinRoom, { foreignKey: 'roomId'})
+  JoinRoom.belongsTo(Rooms, { foreignKey: 'roomId'})
+
+  const res = await JoinRoom.findAll({ where: request, include: [{model: Rooms, required: true}]})
+  const resRooms = res.map( (room) => {
+    return {...room['dataValues'], room: room['room']['dataValues']}
+  })
   return res.length == 0 ? null : resRooms
 }
 
