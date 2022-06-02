88c013bbd6fb9ff4aff33899d1970fdedf70eb45
KanG98 <yankang198@gmail.com>
2022-06-01 21:32:14 -0400

    change roomId to 4 digits char

--- database/DMLs/insertRoom.js
+++ database/DMLs/insertRoom.js
@@ -1,14 +1,34 @@
-const uuid = require('uuid')
+const getRoom = require('./getRoom')
+function generateRoomId(){
+  //generate 4 digit [0-9A-Z]
+  var result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 4; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
 
-function insertRoom(newRoom){
+async function insertRoom(newRoom){
   const Rooms = require("../rooms")
-  newRoom['roomId'] = uuid.v4() // can be done in module
+  const randomRoomId = generateRoomId()
+
+  const existingRoom = await getRoom(randomRoomId)
+  while(existingRoom != null){
+    //handle duplicate room id
+    randomRoomId = generateRoomId()
+    existingRoom = await getRoom(randomRoomId)
+  }
+
+  newRoom['roomId'] = randomRoomId
+
   Rooms.create(newRoom)
     .then((room) => {
       console.log(`new room record ${newRoom.roomId} created successful`)
     })
     .catch((err) => {
-      console.log(err) // handle duplicate key error here // userid
+      console.log(err)
     });
 }
 
